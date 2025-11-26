7b41b58eba7cb35e6d6d4bdd92b87f0065f1247a
Tyler Onstad-Cran <tonstadcran@gmail.com>
2025-11-26 10:18:28 -0800

    tokens defined

--- src/lexing/token.rs
+++ src/lexing/token.rs
@@ -1,3 +1,6 @@
+//come back later, add more types, remove unnecessary string typing
+
+#[derive(Debug)]
 pub enum token {
     //keywords
     Print(String),
@@ -26,4 +29,57 @@ pub enum token {
     //logical operator
     GreaterThan(String),
     LessThan(String),
+}
+
+impl Token {
+    pub fn get_token(token_type: &str, value: Option<&str>) -> Token {
+        //note that once unnecessary string typing is removed, this will change.
+        match token_type {
+            "Print" => Token::Print("print".to_string()),
+            "Int" => Token::Int("int".to_string()),
+            "If" => Token::If("if".to_string()),
+            "Else" => Token::Else("else".to_string()),
+            "IntegerLiteral" => Token::IntegerLiteral(match value {
+                Some(v) => v.parse::<i32>().unwrap(),
+                None => panic!("IntegerLiteral missing value"),
+            }),
+            "StringLiteral" => Token::StringLiteral(match value {
+                Some(v) => v.to_string(),
+                None => panic!("StringLiteral missing value"),
+            }),
+            "Identifier" => Token::Identifier(match value {
+                Some(v) => v.to_string(),
+                None => panic!("Identifier missing value"),
+            }),
+            "Plus" => Token::Plus("+".to_string()),
+            "Assign" => Token::Assign("=".to_string()),
+            "Semicolon" => Token::Semicolon(";".to_string()),
+            "LeftParen" => Token::LeftParen("(".to_string()),
+            "RightParen" => Token::RightParen(")".to_string()),
+            "LeftBrace" => Token::RightBrace("{".to_string()),
+            "RightBrace" => Token::LeftBrace("}".to_string()),
+            _ => panic!("Token does not match a defined type: Token {}", token_type),
+        }
+    }
+
+    pub fn get_token_regex(token_type: &str) -> String {
+        match token_type {
+            "Print" => r"print",
+            "Int" => r"int",
+            "If" => r"if",
+            "Else" => r"else",
+            "IntegerLiteral" => r"\d+",
+            "StringLiteral" => r#"\".*\""#,
+            "Identifier" => r"[a-zA-Z_][a-zA-Z0-9_]* =",
+            "Plus" => r"+",
+            "Assign" => r"=",
+            "Semicolon" => r";",
+            "LeftParen" => r"(",
+            "RightParen" => r")",
+            "LeftBrace" => r"{",
+            "RightBrace" => r"}",
+            _ => panic!("Token does not match defined type: Token {}", token_type),
+        }
+        .to_string()
+    }
 }
