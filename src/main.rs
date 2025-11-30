fee28bc3418f36cf4573f00ccb34f9962850745b
Tyler Onstad-Cran <tonstadcran@gmail.com>
2025-11-29 23:51:36 -0700

    rework to handle user program input or CLI interface

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,41 @@ if (z > 10) {
 }
 ";
 
-fn main() {
-    let tokens: Vec<Token> = lex_program(PROGRAM);
+fn process_args(): Option<String> {
+    let args: Vec<String> = env::args().collect();
+    let mut optional_program: Option<String> = None;
+    if args.len == 2 {
+        program = args[1];
+    }
+    return program;
+}
 
+//file input (make sure to actually extract the text from the file)
+fn run_file(program: String) {
+    let tokens: Vec<Token> = lex_program(program);
+    
     for token in tokens.iter() {
         println!("{:?}", token);
+    }
+}
+
+//CLI listening
+fn run_prompt() {
+    let mut input = String::new();
+
+    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    let tokens: Vec<Token> = lex_program(input);
+    
+    for token in tokens.iter() {
+        println!("{:?}", token);
+    }
+}
+
+fn main() {
+    let program: String = process_args();
+    match program {
+        case Some(p) => run_file(p);
+        case None => run_prompt();
     }
 }
