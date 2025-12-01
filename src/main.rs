36102abd337c8396130f5e108ac9d460971cba3d
Tyler Onstad-Cran <tonstadcran@gmail.com>
2025-11-30 20:59:25 -0700

    CLI listening works, but not continuous

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,21 @@ mod lexing;
 
 use lexing::lexer::lex_program;
 use lexing::token::Token;
+use std::env;
 
-const PROGRAM: &str = "
-int x = 5;
-int y = 6;
-int z = x + y;
-
-if (z > 10) {
-    print(\"Hello, world!\");
-} else {
-    print(\"Goodbye, world!\");
-}
-";
-
-fn process_args(): Option<String> {
+fn process_args() -> Option<String> {
     let args: Vec<String> = env::args().collect();
     let mut optional_program: Option<String> = None;
-    if args.len == 2 {
-        program = args[1];
+    if args.len() == 2 {
+        optional_program = Some(args[1].clone());
     }
-    return program;
+    return optional_program;
 }
 
 //file input (make sure to actually extract the text from the file)
-fn run_file(program: String) {
+fn run_file(program: &str) {
     let tokens: Vec<Token> = lex_program(program);
-    
+
     for token in tokens.iter() {
         println!("{:?}", token);
     }
@@ -37,19 +26,21 @@ fn run_file(program: String) {
 fn run_prompt() {
     let mut input = String::new();
 
-    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
 
-    let tokens: Vec<Token> = lex_program(input);
-    
+    let tokens: Vec<Token> = lex_program(&input);
+
     for token in tokens.iter() {
         println!("{:?}", token);
     }
 }
 
 fn main() {
-    let program: String = process_args();
+    let program: Option<String> = process_args();
     match program {
-        case Some(p) => run_file(p);
-        case None => run_prompt();
+        Some(p) => run_file(&p),
+        None => run_prompt(),
     }
 }
