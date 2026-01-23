9659fd85c26f1b547eb9b001052793dd03fbedf8
Tyler Onstad-Cran <tonstadcran@gmail.com>
2026-01-22 19:12:10 -0700

    binary op alsmot working

--- src/interpreting/interpreter.rs
+++ src/interpreting/interpreter.rs
@@ -24,36 +24,85 @@ impl<'a> Interpreter<'a> {
         self.evaluate(exp)
     }
 
-    fn eval_binary(&mut self, Expr::Binary { left, op, right }: Binary) -> Result<Value, RunTimeError> {
+    fn is_equal(&mut self, v1: Value, v2: Value) -> Result<bool, RunTimeError> {
+        match (v1, v2) {
+            (Value::Number(n1), Value::Number(n2)) => Ok(n1 == n2),
+            (Value::String(s1), Value::String(s2)) => Ok(s1 == s2),
+            (Value::Boolean(b1), Value::Boolean(b2)) => Ok(b1 == b2),
+            (_, _) => Err(RunTimeError::CouldNotEval("==".to_string())),
+        }
+    }
+
+    fn eval_binary(
+        &mut self,
+        Expr::Binary { left, op, right }: Binary,
+    ) -> Result<Value, RunTimeError> {
         let left = match self.evaluate(*left) {
             Ok(val) => val,
             Err(e) => return Err(e),
         };
         let right = match self.evaluate(*right) {
             Ok(val) => val,
-            Err(e) => return Err(e)        
+            Err(e) => return Err(e),
         };
         //maybe go other way and match left and right types then define possible operations
         match op {
-            BinaryOp::Minus => Ok(right - left),
-            BinaryOp::Slash => Ok(right / left),
-            BinaryOp::Star => Ok(right * left),
-            BinaryOp::Plus => ,
-
+            BinaryOp::Minus => match (left, right) {
+                (Value::Number(n1), Value::Number(n2)) => Ok(Value::Number(n1 - n2)),
+                (_, _) => Err(RunTimeError::CouldNotEval("minus".to_string())),
+            },
+            BinaryOp::Slash => match (left, right) {
+                (Value::Number(n1), Value::Number(n2)) => Ok(Value::Number(n1 / n2)),
+                (_, _) => Err(RunTimeError::CouldNotEval("slash".to_string())),
+            },
+            BinaryOp::Star => match (left, right) {
+                (Value::Number(n1), Value::Number(n2)) => Ok(Value::Number(n1 * n2)),
+                (_, _) => Err(RunTimeError::CouldNotEval("star".to_string())),
+            },
+            BinaryOp::Plus => match (left, right) {
+                //can add additional conversions and abilities in this later
+                (Value::Number(n1), Value::Number(n2)) => Ok(Value::Number(n1 + n2)),
+                (Value::String(s1), Value::String(s2)) => Ok(Value::String(s1 + &s2)),
+                (_, _) => Err(RunTimeError::CouldNotEval("plus".to_string())),
+            },
+            BinaryOp::GreaterThan => match (left, right) {
+                (Value::Number(n1), Value::Number(n2)) => Ok(Value::Boolean(n1 > n2)),
+                (_, _) => Err(RunTimeError::CouldNotEval(">".to_string())),
+            },
+            BinaryOp::GreaterEqual => match (left, right) {
+                (Value::Number(n1), Value::Number(n2)) => Ok(Value::Boolean(n1 >= n2)),
+                (_, _) => Err(RunTimeError::CouldNotEval(">=".to_string())),
+            },
+            BinaryOp::LessThan => match (left, right) {
+                (Value::Number(n1), Value::Number(n2)) => Ok(Value::Boolean(n1 < n2)),
+                (_, _) => Err(RunTimeError::CouldNotEval("<".to_string())),
+            },
+            BinaryOp::LessEqual => match (left, right) {
+                (Value::Number(n1), Value::Number(n2)) => Ok(Value::Boolean(n1 <= n2)),
+                (_, _) => Err(RunTimeError::CouldNotEval(">".to_string())),
+            },
+            BinaryOp::EqualEqual => match self.is_equal(left, right) {
+                Ok(b) => Ok(Value::Boolean(b)),
+                Err(e) => Err(e),
+            },
+            BinaryOp::BangEqual => match self.is_equal(left, right) {
+                Ok(b) => Ok(Value::Boolean(!b)),
+                Err(e) => Err(e),
+            },
         }
     }
 
-    fn eval_unary(
-        &mut self,
-        Expr::Unary { exp, op }: crate::Expr,
-    ) -> Result<Value, RunTimeError> {
+    fn eval_unary(&mut self, Expr::Unary { exp, op }: crate::Expr) -> Result<Value, RunTimeError> {
         let right = match self.evaluate(*exp) {
             Ok(val) => val,
             Err(e) => return Err(e),
         };
 
         match op {
-            UnaryOp::Minus => Ok(Value::Number(-right)),
+            UnaryOp::Minus => match right {
+                Value::Number(n) => Ok(Value::Number(-n)),
+                _ => Err(RunTimeError::CouldNotEval("- unary".to_string())),
+            },
             UnaryOp::Bang => Ok(Value::Boolean(!self.is_truthy(right))),
         }
     }
